bfd95f3c1938581764ba9e97731acc7e9653b2d5
Perrychen25 <perrychen25@gmail.com>
2018-01-31 12:10:20 +0800

    Add devise and pin / user association

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -2,16 +2,16 @@ class PinsController < ApplicationController
 	before_action :find_pin , only: [:show, :edit, :update, :destroy]
 
 	def index
-		@pins= Pin.all.order("created_at DESC" ) 
+		@pins = Pin.all.order("created_at DESC" ) 
 	end
 
 
 	def new
-		@pin = Pin.new
+		@pin = current_user.pins.build
 	end
 
 	def create
-		@pin = Pin.new(pin_params)
+		@pin = current_user.pins.build(pin_params)
 
 		if @pin.save
 			redirect_to @pin, notice: "Successfully created new Pin!"
